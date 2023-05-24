46a598f0c809c83ea6232aa00897a64b2729daa3
Giovanny Vargas <yovaz05@gmail.com>
2023-05-23 19:15:07 -0500

    clase 23-05-23

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { Container } from '@mui/material';
+import { useState } from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import UserContext from './context/UserContext';
+
+const initialUser = { id: 1, name: 'Giovanny' };
+
 const App = () => {
-	const user = { name: 'Gioavnny' };
-	const data = { user };
+	const [user, setUser] = useState(initialUser);
+	const login = () => {
+		setUser(initialUser);
+	};
+	const logout = () => {
+		setUser(null);
+	};
+	const data = { user, login, logout };
 	return (
 		<Container>
 			<UserContext.Provider value={data}>
