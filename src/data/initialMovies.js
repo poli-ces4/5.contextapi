0176d314c5220c294c4d68870111e09e69f893dc
Giovanny Vargas <yovaz05@gmail.com>
2023-05-23 19:40:29 -0500

    clase 23-05-23

---
+++ src/data/initialMovies.js
@@ -0,0 +1,34 @@
+const initialMovies = [
+	{
+		id: 1,
+		title: 'Mario Bross 1',
+		imageURL:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKgLAJEzoF-vhxRkhJXOnRUDAAAKx2mLKq3ROWjrk9n9UfAGX53PGktTu4VIMj0bwypxc&usqp=CAU',
+	},
+	{
+		id: 2,
+		title: 'Mario Bross 2',
+		imageURL:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKgLAJEzoF-vhxRkhJXOnRUDAAAKx2mLKq3ROWjrk9n9UfAGX53PGktTu4VIMj0bwypxc&usqp=CAU',
+	},
+	{
+		id: 3,
+		title: 'Mario Bross 3',
+		imageURL:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKgLAJEzoF-vhxRkhJXOnRUDAAAKx2mLKq3ROWjrk9n9UfAGX53PGktTu4VIMj0bwypxc&usqp=CAU',
+	},
+	{
+		id: 4,
+		title: 'Mario Bross 4',
+		imageURL:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKgLAJEzoF-vhxRkhJXOnRUDAAAKx2mLKq3ROWjrk9n9UfAGX53PGktTu4VIMj0bwypxc&usqp=CAU',
+	},
+	{
+		id: 5,
+		title: 'Mario Bross 5',
+		imageURL:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKgLAJEzoF-vhxRkhJXOnRUDAAAKx2mLKq3ROWjrk9n9UfAGX53PGktTu4VIMj0bwypxc&usqp=CAU',
+	},
+];
+
+export default initialMovies;
